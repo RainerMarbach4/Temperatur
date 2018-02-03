16efb132503801137ced76e94f00037dd41a15ad
Rainer Langheiter <rainer@langheiter.at>
2018-02-03 18:03:36 +0100

    config example

---
+++ config/config-example.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainer
+ * Date: 24.03.2017
+ * Time: 16:58
+ */
+
+$config = array(
+    "servername" => "",
+    "username" => "",
+    "password" => "",
+    "dbname" => ""
+);
+
+$queries = array(
+    "aktuell" => "AktuelleWerte",
+    "aktTag" => "MinMax-aktuellerTag",
+    //"aktWoche" => "MinMax-aktuelleWoche",
+    //"aktMonat" => "MinMax-aktuellerMonat",
+    "aktJahr" => "MinMax-aktuellesJahr",
+    "alles  " => "MinMax-Alles",
+    //"MMtage" => "MinMaxDay",
+    "Pflanzplan"=>"V_Pflanzplan"
+);
